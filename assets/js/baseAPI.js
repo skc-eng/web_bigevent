362de4e2314a2981999a8b20127406dcd5e5d0a1
shenkecheng <2021639976@qq.com>
2021-09-07 10:11:46 +0800

    主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,19 @@
-$(function() {
-    $.ajaxPrefilter(function(options) {
+$(function () {
+    $.ajaxPrefilter(function (options) {
+        //根目录拼接
         options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+        //headers请求头
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+        options.complete = function (res) {
+            console.log(res);
+            if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+                console.log('失败了');
+                location.href = '/login.html'
+            }
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+$(function () {
+    getUserInfo();
+})
+
+var layer = layui.layer
+
+$("#btnLogout").on('click', function () {
+    layer.confirm('确定退出登录?', {
+        icon: 3,
+        title: '提示'
+    }, function (index) {
+        //do something
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+        //关闭询问框
+        layer.close(index);
+    });
+
+})
+
+//获取用户信息
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        success: function (res) {
+            // console.log(res);
+            if (res.status !== 0) return layui.layer.msg('获取用户信息失败！')
+            renderAvatar(res.data);
+        },
+    });
+}
+
+//渲染头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;' + name);
+    //用户头像,没有图片就用第一个字母或汉字
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hiden()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
